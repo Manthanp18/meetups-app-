d46349ae159f5c6a1d5c433c23fb8ab331fc8c2e
Manthanp18 <patelmanthan1806@gmail.com>
2021-05-11 09:16:21 +0530

    done

---
+++ pages/api/new-meetup.js
@@ -0,0 +1,26 @@
+import { MongoClient } from 'mongodb';
+
+// /api/new-meetup
+// POST /api/new-meetup
+
+async function handler(req, res) {
+  if (req.method === 'POST') {
+    const data = req.body;
+
+    const client = await MongoClient.connect(
+    'mongodb+srv://manthan:manthan123@cluster0.0tuvy.mongodb.net/reactnext?retryWrites=true&w=majority');
+    const db = client.db();
+
+    const meetupsCollection = db.collection('meetups');
+
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: 'Meetup inserted!' });
+  }
+}
+
+export default handler;
